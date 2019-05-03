4af7a09061864ca97786599ab7edea4d6fc1eb5d
henmadx <hendyaugustama@gmail.com>
2019-05-03 21:42:27 +0800

    fix compile

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -68,17 +68,6 @@ void mdss_dsi_ulps_suspend_enable(bool enable)
 }
 EXPORT_SYMBOL(mdss_dsi_ulps_suspend_enable);
 
-#ifdef CONFIG_LAZYPLUG
-extern void lazyplug_enter_lazy(bool enter, bool video);
-#endif
-
-bool display_on = true;
-
-bool is_display_on()
-{
-	return display_on;
-}
-
 void mdss_dsi_panel_pwm_cfg(struct mdss_dsi_ctrl_pdata *ctrl)
 {
 	if (ctrl->pwm_pmi)
@@ -921,15 +910,6 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
 
-#ifdef CONFIG_LAZYPLUG
-	lazyplug_enter_lazy(false, false);
-#endif
-
-	/* Ensure low persistence is disabled */
-	//mdss_dsi_panel_apply_display_setting(pdata, 0);
-
-	display_on = true;
-
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
@@ -1061,13 +1041,6 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 		mdss_dsi_panel_cmds_send(ctrl, &ctrl->off_cmds, CMD_REQ_COMMIT);
 
 	mdss_dsi_panel_off_hdmi(ctrl, pinfo);
-
-#ifdef CONFIG_LAZYPLUG
-	lazyplug_enter_lazy(true, false);
-#endif
-
-
-	display_on = false;
 
 end:
 	/* clear idle state */
